7eb0be8224cd89c1c4573931d198ba23c11d54ac
Abel Goncalo <abelgoncalo8@gmail.com>
2025-04-21 02:07:27 +0100

    feat: add servicecontroller and view service

---
+++ resources/views/servicos/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Serviços')
+
+@section('content_header')
+    <h1>Serviçoes</h1>
+@stop
+
+@section('content')
+    <p>Serviços E-diarista.</p>
+@stop
